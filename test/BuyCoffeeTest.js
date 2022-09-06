0aef8d56c3dd00d41410b7c3fe9c506b08199d76
royvardhan <royvardhan.official@gmail.com>
2022-09-06 10:56:12 +0530

    writing tests

---
+++ test/BuyCoffeeTest.js
@@ -0,0 +1,41 @@
+const { assert } = require("chai");
+const { utils } = require("ethers");
+const { ethers } = require("hardhat");
+
+describe("BuyRoyACoffee", () => {
+  let coffeeFactory;
+  let buyRoyACoffee;
+
+  beforeEach(async () => {
+    coffeeFactory = await ethers.getContractFactory("BuyRoyACoffee");
+    buyRoyACoffee = await coffeeFactory.deploy();
+  });
+
+  it("Should show the current balance of the contract as 0", async () => {
+    const currentBalance = await buyRoyACoffee.showBalance();
+    const expectedBalance = 0;
+    assert.equal(currentBalance, expectedBalance);
+  });
+
+  it("Should update the balance of the contract after recieving a tip", async () => {
+    const tipAmount = utils.parseEther("0.1");
+    const name = "Roy";
+    const message = "writingTest";
+    const tx = await buyRoyACoffee.buyCoffee(tipAmount, name, message, {
+      value: tipAmount,
+    });
+    const balance = await buyRoyACoffee.showBalance();
+    assert.equal(balance.toString(), tipAmount.toString());
+  });
+
+  it("Shall display the keys in the memo", async () => {
+    const tipAmount = utils.parseEther("0.1");
+    const name = "Roy";
+    const message = "writingTest";
+    const tx = await buyRoyACoffee.buyCoffee(tipAmount, name, message, {
+      value: tipAmount,
+    });
+    const res = await buyRoyACoffee.memos()[0].name;
+    assert.equal(name, resName);
+  });
+});
